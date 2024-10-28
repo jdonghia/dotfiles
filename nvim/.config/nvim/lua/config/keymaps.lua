41e229084b7abab784922ac37714fee3c440ac74
João <joao.donghia@gmail.com>
2024-10-28 08:57:36 -0300

    build(global): update preferences

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
 
+vim.keymap.set("n", "<leader>-", "<cmd>vsp<CR>")
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- vim.keymap.set("n", "J", "mzJ`z")
