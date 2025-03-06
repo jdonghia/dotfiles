aea58ad75c450a4896b65e1e428f27efe21276d4
João <joao.donghia@gmail.com>
2025-03-06 19:40:31 -0300

    feat: include windows and arch shell scripts

--- ascript.sh
+++ ascript.sh
@@ -5,7 +5,7 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-yay -S stow tmux waybar neovim hyprshot swaync ttf-font-awesome ttf-jetbrains-mono-nerd pnpm nodejs wofi hyprpaper xclip zoxide fzf firefox ripgrep lazygit hyprland kitty zsh zsh-theme-powerlevel10k-git
+yay -S stow tmux waybar neovim hyprshot swaync ttf-font-awesome ttf-jetbrains-mono-nerd pnpm nodejs wofi hyprpaper xclip zoxide fzf firefox ripgrep lazygit hyprland kitty zsh zsh-theme-powerlevel10k-git npm openssh unzip
 
 git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -13,6 +13,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 chsh -s /usr/bin/zsh
 
 git clone https://www.github.com/jdonghia/dotfiles.git
+cd ~/dotfiles
 
 stow backgrounds hypr kitty nvim tmux waybar zshrc
 
---
+++ wscript.sh
@@ -0,0 +1,21 @@
+#!/bin/bash  
+
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+yay -S stow tmux neovim pnpm nodejs zoxide fzf firefox ripgrep lazygit zsh zsh-theme-powerlevel10k-git npm openssh unzip
+
+git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+chsh -s /usr/bin/zsh
+
+git clone https://www.github.com/jdonghia/dotfiles.git
+cd ~/dotfiles
+
+stow nvim tmux zshrc
+
+sudo pacman -Syyuu
+
