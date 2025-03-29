8857bfb467c31b9c3be0795336707f55719fb8b8
João <joao.donghia@gmail.com>
2025-03-29 14:42:12 -0300

    build: update sh script

--- ascript.sh
+++ ascript.sh
@@ -2,20 +2,23 @@
 
 cd ~
 
-sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git 
 cd yay
 makepkg -si
 
-yay -S stow tmux waybar neovim hyprshot swaync ttf-font-awesome ttf-jetbrains-mono-nerd pnpm nodejs wofi hyprpaper xclip zoxide fzf firefox ripgrep lazygit hyprland kitty zsh zsh-theme-powerlevel10k-git npm openssh unzip
+yay -S stow tmux waybar neovim hyprshot swaync ttf-font-awesome ttf-jetbrains-mono-nerd pnpm nodejs wofi hyprpaper xclip zoxide fzf firefox ripgrep lazygit zsh starship npm openssh unzip bluetuith pavucontrol google-chrome bluez bluez-utils 
 
-git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+sudo systemctl enable bluetooth.service
+sudo systemctl start bluetooth.service
+
+# git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 chsh -s /usr/bin/zsh
 
-git clone https://www.github.com/jdonghia/dotfiles.git
-cd dotfiles
+cd ~/dotfiles
+
+rm -rf ~/.config/hypr/
 
 stow backgrounds hypr kitty nvim tmux waybar zshrc tmux-sessionizer
 
@@ -25,5 +28,5 @@ source ~/.zshrc
 
 timedatectl set-timezone America/Sao_Paulo
 
-sudo pacman -Syu
+yay -Syu
 
