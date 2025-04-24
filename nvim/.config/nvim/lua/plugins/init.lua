c9fc8000cbb5e7af9a81dd15c59d71938d6c5732
João <joao.donghia@gmail.com>
2025-04-24 10:24:07 -0300

    chore(nvim): update colorscheme

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -10,11 +10,11 @@ return {
   -- { 'xiyaowong/transparent.nvim', config = function() end },
 
   {
-    'tanvirtin/monokai.nvim',
+    'rebelot/kanagawa.nvim',
     config = function()
-      -- vim.cmd 'colorscheme monokai_soda'
-      vim.cmd 'colorscheme monokai_pro'
 
+      -- setup must be called before loading
+      vim.cmd 'colorscheme kanagawa'
       -- vim.api.nvim_set_hl(0, 'normal', { bg = 'none' })
       -- vim.api.nvim_set_hl(0, 'normalfloat', { bg = 'none' })
     end,
