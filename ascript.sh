886f0fc4b40bc50f57475e8dad76dd4753be7068
João <joao.donghia@gmail.com>
2025-04-24 10:23:33 -0300

    build(shell-script): remove unused packages

--- ascript.sh
+++ ascript.sh
@@ -6,10 +6,12 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-yay -S stow tmux waybar neovim hyprshot swaync ttf-font-awesome ttf-jetbrains-mono-nerd pnpm nodejs wofi hyprpaper xclip zoxide fzf firefox ripgrep lazygit zsh starship npm openssh unzip bluetuith pavucontrol google-chrome bluez bluez-utils 
+yay -S stow tmux neovim ttf-font-awesome ttf-jetbrains-mono-nerd pnpm nodejs xclip zoxide fzf firefox ripgrep lazygit zsh starship npm openssh unzip 
 
-sudo systemctl enable bluetooth.service
-sudo systemctl start bluetooth.service
+# yay -S stow tmux waybar neovim hyprshot swaync ttf-font-awesome ttf-jetbrains-mono-nerd pnpm nodejs wofi hyprpaper xclip zoxide fzf firefox ripgrep lazygit zsh starship npm openssh unzip bluetuith pavucontrol google-chrome bluez bluez-utils 
+
+# sudo systemctl enable bluetooth.service
+# sudo systemctl start bluetooth.service
 
 # git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
 # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -18,15 +20,17 @@ chsh -s /usr/bin/zsh
 
 cd ~/dotfiles
 
-rm -rf ~/.config/hypr/
+# rm -rf ~/.config/hypr/
 
-stow backgrounds hypr kitty nvim tmux waybar zshrc tmux-sessionizer
+# stow backgrounds hypr kitty nvim tmux waybar zshrc tmux-sessionizer
+
+stow nvim tmux zshrc tmux-sessionizer
 
 chmod +x ~/.local/scripts/tmux-sessionizer
 
 source ~/.zshrc
 
-timedatectl set-timezone America/Sao_Paulo
+# timedatectl set-timezone America/Sao_Paulo
 
 yay -Syu
 
