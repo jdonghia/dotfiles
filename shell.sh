8bd760ac30a0284c5284781fb91665958956fdc9
João <joao.donghia@gmail.com>
2025-04-25 19:31:34 -0300

    update shell script

--- shell.sh
+++ shell.sh
@@ -4,14 +4,36 @@ cd ~
 git clone https://aur.archlinux.org/yay.git 
 cd yay
 makepkg -si
-yay -S stow tmux neovim ttf-jetbrains-mono-nerd pnpm nodejs xclip zoxide fzf firefox ripgrep lazygit zsh starship npm openssh unzip 
-git clone https://www.github.com/jdonghia/dotfiles.git
+
+packages=(
+    stow
+    tmux
+    neovim
+    ttf-jetbrains-mono-nerd
+    pnpm
+    nodejs
+    xclip
+    zoxide
+    fzf
+    firefox
+    ripgrep
+    lazygit
+    zsh
+    starship
+    npm
+    openssh
+    unzip
+)
+
+for package in ${packages[@]}; do
+    yay -S --noconfirm ${package}
+done
+
 chsh -s /usr/bin/zsh
-cd ~/dotfiles
-stow nvim tmux zshrc tmux-sessionizer
-chmod +x ~/.local/scripts/tmux-sessionizer
-source ~/.zshrc
+
 sudo pacman -Syu
+
+# chmod +x ~/.local/scripts/tmux-sessionizer
 
 # yay -S stow tmux waybar neovim hyprshot swaync ttf-font-awesome ttf-jetbrains-mono-nerd pnpm nodejs wofi hyprpaper xclip zoxide fzf firefox ripgrep lazygit zsh starship npm openssh unzip bluetuith pavucontrol google-chrome bluez bluez-utils 
 # sudo systemctl enable bluetooth.service
