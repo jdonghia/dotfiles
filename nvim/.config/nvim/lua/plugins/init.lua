8e1ad512774e1e7bdd1f564b1757a6c150e338c0
João Donghia <joao.donghia@imusica.com.br>
2025-04-28 12:48:59 -0300

    build(nvim): remove colorscheme

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -32,15 +32,15 @@ return {
     opts = {},
   },
 
-  {
-    'projekt0n/github-nvim-theme',
-    name = 'github-theme',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd 'colorscheme github_dark'
-    end,
-  },
+  -- {
+  --   'projekt0n/github-nvim-theme',
+  --   name = 'github-theme',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd 'colorscheme github_dark'
+  --   end,
+  -- },
 
   -- {
   --   'rebelot/kanagawa.nvim',
