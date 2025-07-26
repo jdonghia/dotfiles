dd8a999941824cc050405f1b1c289d9208afa51a
João <joao.donghia@gmail.com>
2025-07-25 22:00:08 -0300

    nvim: update ui

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,8 +1,23 @@
 return {
   'folke/noice.nvim',
   event = 'VeryLazy',
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    {
+      'rcarriga/nvim-notify',
+      config = function()
+        require('notify').setup {
+          background_colour = '#000000',
+          timeout = 1500,
+        }
+      end,
+    },
+  },
   opts = {
     cmdline = {
+      enable = true,
+    },
+    messages = {
       enable = true,
     },
   },
