4623025c0a5c7dbefc454cc730bc0180656c2853
João <joao.donghia@gmail.com>
2025-07-27 12:06:16 -0300

    nvim: disable snippets/ enable completions

--- nvim/.config/nvim/lua/plugins/snippets.lua
+++ nvim/.config/nvim/lua/plugins/snippets.lua
@@ -1,73 +1,42 @@
 return {
-  {
-    'hrsh7th/cmp-nvim-lsp',
-  },
-  {
-    'L3MON4D3/LuaSnip',
-    dependencies = {
-      'saadparwaiz1/cmp_luasnip',
-      'rafamadriz/friendly-snippets',
-    },
-    config = function()
-      local luasnip = require 'luasnip'
-
-      require('luasnip.loaders.from_vscode').lazy_load()
-
-      luasnip.filetype_extend('javascript', { 'html' })
-      luasnip.filetype_extend('javascriptreact', { 'html' })
-      luasnip.filetype_extend('typescript', { 'html' })
-      luasnip.filetype_extend('typescriptreact', { 'html' })
-    end,
-  },
-  {
-    'hrsh7th/nvim-cmp',
-    version = false,
-    config = function()
-      local cmp = require 'cmp'
-
-      cmp.setup {
-        snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert {
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm { select = true },
-          -- Add tab support for snippet expansion
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif require('luasnip').expand_or_jumpable() then
-              require('luasnip').expand_or_jump()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif require('luasnip').jumpable(-1) then
-              require('luasnip').jump(-1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-        },
-        sources = cmp.config.sources({
-          { name = 'luasnip' },
-          { name = 'nvim_lsp' },
-        }, {
-          { name = 'buffer' },
-        }),
-      }
-    end,
-  },
+  --
+  -- {
+  --   'hrsh7th/cmp-nvim-lsp',
+  -- },
+  -- {
+  --   'L3MON4D3/LuaSnip',
+  --   dependencies = {
+  --     'saadparwaiz1/cmp_luasnip',
+  --     'rafamadriz/friendly-snippets',
+  --   },
+  --   config = function()
+  --     -- local luasnip = require 'luasnip'
+  --
+  --
+  --     -- luasnip.filetype_extend('javascriptreact', { 'html' })
+  --     -- luasnip.filetype_extend('typescriptreact', { 'html' })
+  --   end,
+  -- },
+  -- {
+  --   'hrsh7th/nvim-cmp',
+  --   version = false,
+  --   config = function()
+  --     local cmp = require 'cmp'
+  --
+  --     require('luasnip.loaders.from_vscode').lazy_load()
+  --     cmp.setup {
+  --       snippet = {
+  --         expand = function(args)
+  --           require('luasnip').lsp_expand(args.body)
+  --         end,
+  --       },
+  --       sources = cmp.config.sources({
+  --         { name = 'luasnip' },
+  --         { name = 'nvim_lsp' },
+  --       }, {
+  --         { name = 'buffer' },
+  --       }),
+  --     }
+  --   end,
+  -- },
 }
