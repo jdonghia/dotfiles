76eecebe1f850f296a9779aafa193872d4da6220
João <joao.donghia@gmail.com>
2025-07-27 15:00:29 -0300

    feat(lsp): code action keymap

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -19,43 +19,49 @@ return {
       local capabilities = require('blink.cmp').get_lsp_capabilities()
 
       require('lspconfig').ts_ls.setup { capabilities = capabilities }
-      require('lspconfig').lua_ls.setup {}
-      require('lspconfig').tailwindcss.setup {}
-      require('lspconfig').gopls.setup {}
-      require('lspconfig').marksman.setup {}
-      -- require('lspconfig').mdx_analyzer.setup {}
-      -- require('lspconfig').bashls.setup {}
-      -- require('lspconfig').eslint.setup {}
+      require('lspconfig').eslint.setup { capabilities = capabilities }
+      require('lspconfig').lua_ls.setup { capabilities = capabilities }
+      require('lspconfig').tailwindcss.setup { capabilities = capabilities }
+      require('lspconfig').gopls.setup { capabilities = capabilities }
+      require('lspconfig').marksman.setup { capabilities = capabilities }
+      require('lspconfig').mdx_analyzer.setup { capabilities = capabilities }
+      require('lspconfig').bashls.setup { capabilities = capabilities }
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = args.buf, desc = 'Code Action' })
+        end,
+      })
     end,
   },
 
   {
     'williamboman/mason.nvim',
-    config = function()
-      require('mason').setup {}
-    end,
     dependencies = {
-
       {
         'WhoIsSethDaniel/mason-tool-installer.nvim',
         config = function()
           require('mason-tool-installer').setup {
             ensure_installed = {
-              -- 'bash-language-server',
               'stylua',
               'prettierd',
-              'eslint_d',
               'gopls',
-              -- 'eslint-lsp',
               'typescript-language-server',
               'lua-language-server',
               'tailwindcss-language-server',
               'marksman',
-              -- 'mdx-analyzer',
+              'eslint_d',
+              'eslint-lsp',
+              'bash-language-server',
+              'mdx-analyzer',
             },
           }
         end,
       },
     },
+
+    config = function()
+      require('mason').setup {}
+    end,
   },
 }
