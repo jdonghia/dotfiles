29f0eee3c2a9e95edeb2296116fdaf962c051a5d
João Donghia <joao.donghia@imusica.com.br>
2025-07-28 17:43:30 -0300

    nvim(completions): return to nvim-cmp since blink doesnt work with obsidian config

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -1,20 +1,64 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = {
-    'rafamadriz/friendly-snippets',
+  {
+    "hrsh7th/cmp-nvim-lsp"
   },
-  version = '0.*',
-  config = function()
-    require('blink.cmp').setup {
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    version = false,
+    config = function()
+      local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
 
-      keymap = { preset = 'default' },
-
-      appearance = {
-        use_nvim_cmp_as_default = true,
-        nerd_font_variant = 'mono',
-      },
-
-      signature = { enabled = true },
-    }
-  end,
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, 
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+  },
 }
+
+
+
+-- return {
+--   'saghen/blink.cmp',
+--   dependencies = {
+--     'rafamadriz/friendly-snippets',
+--   },
+--   version = '0.*',
+--   config = function()
+--     require('blink.cmp').setup {
+--
+--       keymap = { preset = 'default' },
+--
+--       appearance = {
+--         use_nvim_cmp_as_default = true,
+--         nerd_font_variant = 'mono',
+--       },
+--
+--       signature = { enabled = true },
+--     }
+--   end,
+-- }
