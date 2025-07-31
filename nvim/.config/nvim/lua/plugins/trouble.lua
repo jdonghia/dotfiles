66444dcde0ad5d682735150146ea5ad559892ba0
João Donghia <joao.donghia@imusica.com.br>
2025-07-31 18:57:56 -0300

    nvim: include trouble

---
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,14 @@
+return {
+  'folke/trouble.nvim',
+  config = function()
+    require('trouble').setup {}
+  end,
+  cmd = 'Trouble',
+  keys = {
+    {
+      '<leader>q',
+      '<cmd>Trouble diagnostics toggle<cr>',
+      desc = 'Diagnostics (Trouble)',
+    },
+  },
+}
