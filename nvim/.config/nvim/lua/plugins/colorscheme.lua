93f14c8fa2d5e2969a0a6f1dff13a9c0119be961
João <joao.donghia@gmail.com>
2025-09-23 15:45:50 -0300

    update colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,21 @@
 return {
   {
-    'rose-pine/neovim',
+    -- 'rose-pine/neovim',
+    'ellisonleao/gruvbox.nvim',
     config = function()
-      require('rose-pine').setup {
-        styles = {
-          transparency = true,
-        },
+      -- require('rose-pine').setup {
+      --   styles = {
+      --     transparency = true,
+      --   },
+      --
+      -- }
+
+      require('gruvbox').setup {
+        transparent_mode = true,
       }
 
-      vim.cmd 'colorscheme rose-pine'
+      -- vim.cmd 'colorscheme rose-pine'
+      vim.cmd 'colorscheme gruvbox'
     end,
   },
 }
