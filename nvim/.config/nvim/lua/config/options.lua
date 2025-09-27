19c1ac9353aab2ef0c60d1313ca40beafc7b4738
João <joao.donghia@gmail.com>
2025-09-27 15:26:02 -0300

    remove line wrap

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -13,8 +13,6 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
 vim.opt.undofile = true
 
-vim.opt.wrap = false
-
 vim.opt.updatetime = 300
 
 vim.opt.termguicolors = true
