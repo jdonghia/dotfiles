4b52f95f8825dc1febfebdca8fc3ea469128b611
João <joao.donghia@gmail.com>
2025-10-31 10:04:11 -0300

    update copilot keybind

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -11,8 +11,12 @@ return {
   -- },
   {
     'github/copilot.vim',
-    -- config = function()
-    --   vim.cmd 'Copilot disable'
-    -- end,
+    config = function()
+      vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+      })
+      vim.g.copilot_no_tab_map = true
+    end,
   },
 }
