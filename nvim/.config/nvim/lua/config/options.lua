5c0aa6e53855e473c60f72c30fbcdc3061c37c5b
Joao Donghia <joao.donghia@gmail.com>
2025-11-24 14:30:08 -0300

    update obsidian nvim

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -32,4 +32,6 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
+vim.opt.conceallevel = 2
+
 
--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -1,74 +1,56 @@
 return {
-  -- 'epwalsh/obsidian.nvim',
-  -- lazy = true,
-  -- version = '*',
-  -- ft = 'markdown',
-  -- dependencies = {
-  --   'nvim-lua/plenary.nvim',
-  -- },
-  -- keys = {
-  --   { 'gt', '<cmd>ObsidianTag<CR>', silent = true },
-  --   {
-  --     'gf',
-  --     function()
-  --       return require('obsidian').util.gf_passthrough()
-  --     end,
-  --     mode = 'n',
-  --     noremap = false,
-  --     expr = true,
-  --     buffer = true,
-  --     silent = true,
-  --   },
-  --   {
-  --     '<leader>ti',
-  --     function()
-  --       return require('obsidian').util.toggle_checkbox()
-  --     end,
-  --     mode = 'n',
-  --     buffer = true,
-  --     silent = true,
-  --   },
-  --   {
-  --     '<leader>on',
-  --     ':ObsidianTemplate note<cr> :lua vim.cmd([[1,/^\\S/s/^\\n\\{1,}//]])<cr>',
-  --     mode = 'n',
-  --     silent = true,
-  --   },
-  --   -- {
-  --   --   '<leader>ok',
-  --   --   ":!mv '%:p' $HOME/obsidian/zettelkasten<cr>:bd<cr>",
-  --   --   mode = 'n',
-  --   --   silent = true,
-  --   -- },
-  --   {
-  --     '<leader>odd',
-  --     ":!mkdir -p ~/obsidian/.trash && mv '%:p' ~/obsidian/.trash<cr>:bd<cr>",
-  --     mode = 'n',
-  --     silent = true,
-  --   },
-  -- },
-  --
-  -- config = function()
-  --   vim.opt_local.conceallevel = 2
-  --
-  --   require('obsidian').setup {
-  --     disable_frontmatter = true,
-  --     workspaces = {
-  --       {
-  --         path = '~/obsidian',
-  --       },
-  --     },
-  --     notes_subdir = 'inbox',
-  --     new_notes_location = 'notes_subdir',
-  --     note_id_func = function(title)
-  --       return title
-  --     end,
-  --     templates = {
-  --       subdir = 'templates',
-  --     },
-  --     ui = {
-  --       checkboxes = {},
-  --     },
-  --   }
-  -- end,
+  'obsidian-nvim/obsidian.nvim',
+  version = '*', 
+  ft = 'markdown',
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    legacy_commands = false, 
+    workspaces = {
+      {
+        name = 'personal',
+        path = '~/docs',
+      },
+    },
+    disable_frontmatter = true,
+  },
+
+  keys = {
+    { 'gt', '<cmd>ObsidianTag<CR>', silent = true },
+    {
+      'gf',
+      function()
+        return require('obsidian').util.gf_passthrough()
+      end,
+      mode = 'n',
+      noremap = false,
+      expr = true,
+      buffer = true,
+      silent = true,
+    },
+    {
+      '<leader>ti',
+      function()
+        return require('obsidian').util.toggle_checkbox()
+      end,
+      mode = 'n',
+      buffer = true,
+      silent = true,
+    },
+    {
+      '<leader>odd',
+      ":!mkdir -p ~/obsidian/.trash && mv '%:p' ~/obsidian/.trash<cr>:bd<cr>",
+      mode = 'n',
+      silent = true,
+    },
+    {
+      '<leader>oo',
+
+      function()
+        vim.cmd 'ObsidianOpen'
+      end,
+      mode = 'n',
+      silent = true,
+    },
+  },
 }
