4d6d7f1517f57025dc80631f9e503af28307b4b4
João Donghia <joao.donghia@gmail.com>
2025-12-12 17:53:18 -0300

    remove some options

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,8 +1,6 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.opt.mouse = ''
-
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.hlsearch = false
@@ -15,23 +13,21 @@ vim.opt.undofile = true
 
 -- vim.opt.updatetime = 100
 
-vim.opt.termguicolors = true
-vim.opt.number = true
+-- vim.opt.termguicolors = true
+-- vim.opt.number = true
+-- vim.opt.signcolumn = 'yes'
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
-vim.opt.signcolumn = 'yes'
 vim.opt.scrolloff = 8
 
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.breakindent = true
-vim.opt_local.formatoptions:remove { 'r', 'o' }
 
-
-vim.g.netrw_browse_split = 0
+-- vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
+-- vim.g.netrw_winsize = 25
 
 -- vim.opt.conceallevel = 2
 
