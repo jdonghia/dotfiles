48f4a3d6c2c29a0c62a71c4a80a622cd3c2e2004
Joao Donghia <joao.donghia@gmail.com>
2025-12-28 17:01:00 -0300

    update markdown

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,20 +1,7 @@
 return {
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('render-markdown').setup {
-        file_types = { 'markdown', 'mdx' },
-
-        enabled = true,
-        max_file_size = 10.0,
-        debounce = 100,
-      }
-    end,
-    ft = { 'markdown', 'mdx' },
-  },
-
-  {
-    'davidmh/mdx.nvim',
-  },
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {},
 }
