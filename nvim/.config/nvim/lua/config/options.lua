56b4855ab03aacf1a453bef887bf925ba22eccb9
Joao Donghia <joao.donghia@gmail.com>
2026-01-10 10:42:45 -0300

    relativenumber to number

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -10,7 +10,7 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
 vim.opt.undofile = true
 
-vim.opt.relativenumber = true
+vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.scrolloff = 8
 
