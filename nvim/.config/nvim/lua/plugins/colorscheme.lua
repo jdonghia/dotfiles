e4da83729984ecf1462eb61c353b7e9963b72068
Joao Donghia <joao.donghia@gmail.com>
2026-01-21 22:18:15 -0300

    update colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,11 @@
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  priority = 1000,
-  config = function()
-    vim.cmd 'colorscheme catppuccin-mocha'
-  end,
+  {
+    'projekt0n/github-nvim-theme',
+    name = 'github-theme',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd 'colorscheme github_dark_dimmed'
+    end,
+  },
 }
