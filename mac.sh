d6f5b21b3d43647d3348dd87d559e4fc174dd5e1
João Donghia <joao.donghia@gmail.com>
2026-01-26 10:01:49 -0300

    starship mac.sh

--- mac.sh
+++ mac.sh
@@ -15,6 +15,7 @@ cli_packages=(
     fzf
     zoxide
     gemini-cli
+    starship
     go
     opencode
     starship
@@ -45,6 +46,7 @@ brew install --cask "${cask_packages[@]}"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
