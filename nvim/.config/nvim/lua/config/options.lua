fc60165b05b0199c3bf7045792c4b85d2fb23e50
João Donghia <joao.donghia@gmail.com>
2026-02-06 10:27:02 -0300

    undercurl

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -22,3 +22,9 @@ vim.opt.breakindent = true
 
 vim.o.termguicolors = true
 
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+vim.opt.spell = true
+vim.opt.spelllang = { "en_us" }
+
