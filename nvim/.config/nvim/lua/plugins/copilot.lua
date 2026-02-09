87e406bc6a34345af2fb69e82f4c760be6562bff
João Donghia <joao.donghia@gmail.com>
2026-02-09 14:16:27 -0300

    keymaps for copilot

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -4,8 +4,13 @@ return {
     vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
       expr = true,
       replace_keycodes = false,
-      silent = true
+      silent = true,
     })
+
+    vim.keymap.set('n', '<leader>ce', '<cmd>Copilot enable<CR>')
+    vim.keymap.set('n', '<leader>cd', '<cmd>Copilot disable<CR>')
+
     vim.g.copilot_no_tab_map = true
+    vim.g.copilot_enabled = false
   end,
 }
