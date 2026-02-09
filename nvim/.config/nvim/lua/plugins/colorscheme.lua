0877b347c45b48a6148b4ebfc1008d766fe10cb6
João Donghia <joao.donghia@gmail.com>
2026-02-09 14:16:19 -0300

    adjust colorscheme config

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,8 @@
 return {
   'loctvl842/monokai-pro.nvim',
-  lazy = false,
-  priority = 1000,
   config = function()
     require('monokai-pro').setup {
-      -- transparent_background = true,
+      transparent_background = true,
     }
 
     vim.cmd.colorscheme 'monokai-pro-classic'
