8e210a64781e55d5a8a17f03fe4de8dfe70f0788
Joao Donghia <joao.donghia@gmail.com>
2026-02-11 16:24:02 -0300

    wip

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -55,11 +55,11 @@ return {
       },
       sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' },
+        lualine_b = { 'filename' },
+        lualine_c = { '' },
+        lualine_x = { '' },
+        lualine_y = { 'diagnostics' },
+        lualine_z = { 'branch' },
       },
     }
   end,
