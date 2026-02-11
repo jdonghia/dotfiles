e810fd1c00212f3fe576657d0f8e9d87a3bb53f7
Joao Donghia <joao.donghia@gmail.com>
2026-02-11 18:47:20 -0300

    remove weird background oil

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,5 +1,6 @@
 return {
   'stevearc/oil.nvim',
+  priority = 1000,
   lazy = false,
   config = function()
     require('oil').setup {
@@ -7,5 +8,12 @@ return {
         show_hidden = true,
       },
     }
+
+    -- match dir color with hidden dir color (background oil dir issue)
+    local comment_hl = vim.api.nvim_get_hl(0, { name = 'Comment' })
+
+    vim.api.nvim_set_hl(0, 'OilDir', { fg = comment_hl.fg, bg = 'none' })
+    vim.api.nvim_set_hl(0, 'OilDirHidden', { fg = '#6e6e66' })
+    vim.api.nvim_set_hl(0, 'OilFileHidden', { fg = '#6e6e66' })
   end,
 }
