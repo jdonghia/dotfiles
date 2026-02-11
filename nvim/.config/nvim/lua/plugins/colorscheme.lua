80ab54e7daf392935a8e1c4cccac694890b71887
Joao Donghia <joao.donghia@gmail.com>
2026-02-11 14:35:13 -0300

    enable transparent background

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ return {
   'loctvl842/monokai-pro.nvim',
   config = function()
     require('monokai-pro').setup {
-      -- transparent_background = true,
+      transparent_background = true,
     }
 
     vim.cmd.colorscheme 'monokai-pro-classic'
