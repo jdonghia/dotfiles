8dc57fb2c46464e79355efeff129c1f97c8705ce
Joao Donghia <joao.donghia@gmail.com>
2026-02-11 18:47:10 -0300

    remove tildes

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,28 +1,35 @@
 vim.g.mapleader = ' '
 
+-- colors
+vim.o.termguicolors = true
+
+-- search setup
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+-- undo setup
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
 vim.opt.undofile = true
 
+-- general setup
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
-
 vim.opt.scrolloff = 8
 
+-- identation
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.breakindent = true
 
-vim.o.termguicolors = true
+-- remove tildes from numbers ~
+vim.opt.fillchars = { eob = ' ' }
 
-vim.cmd([[let &t_Cs = "\e[4:3m"]])
-vim.cmd([[let &t_Ce = "\e[4:0m"]])
-
+-- enable undercurl
+vim.cmd [[let &t_Cs = "\e[4:3m"]]
+vim.cmd [[let &t_Ce = "\e[4:0m"]]
