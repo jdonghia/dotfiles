2994eec88862c9d9a04dd6bdab242f9cf3ce4065
Joao Donghia <joao.donghia@gmail.com>
2026-02-18 11:30:58 -0300

    live-preview and markdown neovim

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -17,6 +17,12 @@ return {
     build = 'cd app && npm install',
     init = function()
       vim.g.mkdp_filetypes = { 'markdown' }
+      vim.g.mkdp_browserfunc = 'OpenMarkdownPreview'
+      vim.cmd [[
+        function OpenMarkdownPreview(url)
+          execute "silent ! open -a Zen -n --args --new-window " . a:url
+        endfunction
+      ]]
     end,
     ft = { 'markdown' },
   },
