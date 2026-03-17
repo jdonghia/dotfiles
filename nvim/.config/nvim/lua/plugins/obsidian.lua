c63196b0fbcd4a8814f8210adbed5e3bc1ff43b6
Joao Donghia <joao.donghia@gmail.com>
2026-03-17 16:41:27 -0300

    unable lualine and obsidian

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -1,26 +1,26 @@
 return {
-  'obsidian-nvim/obsidian.nvim',
-  version = '*',
-  ft = 'markdown',
-  config = function()
-    require('obsidian').setup {
-
-      legacy_commands = false,
-      ui = { enable = false },
-      workspaces = {
-        {
-          name = 'obsidian',
-          path = '~/obsidian',
-        },
-      },
-      frontmatter = { enabled = false },
-      statusline = { enabled = false },
-      checkbox = {
-        create_new = false,
-      },
-    }
-  end,
-  keys = {
-    { '<leader>oo', '<cmd>Obsidian open<CR>', silent = true },
-  },
+  -- 'obsidian-nvim/obsidian.nvim',
+  -- version = '*',
+  -- ft = 'markdown',
+  -- config = function()
+  --   require('obsidian').setup {
+  --
+  --     legacy_commands = false,
+  --     ui = { enable = false },
+  --     workspaces = {
+  --       {
+  --         name = 'obsidian',
+  --         path = '~/obsidian',
+  --       },
+  --     },
+  --     frontmatter = { enabled = false },
+  --     statusline = { enabled = false },
+  --     checkbox = {
+  --       create_new = false,
+  --     },
+  --   }
+  -- end,
+  -- keys = {
+  --   { '<leader>oo', '<cmd>Obsidian open<CR>', silent = true },
+  -- },
 }
