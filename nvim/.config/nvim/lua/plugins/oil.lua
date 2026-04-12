cd8e1770628aac6a7bd204adb364a5df61dd860f
Joao Donghia <joao.donghia@gmail.com>
2026-04-12 11:48:17 -0300

    !!nvim: migrate to Lazyvim

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,19 +1,18 @@
 return {
-  'stevearc/oil.nvim',
+  "stevearc/oil.nvim",
   priority = 1000,
-  lazy = false,
   config = function()
-    require('oil').setup {
+    require("oil").setup({
       view_options = {
         show_hidden = true,
       },
-    }
+    })
 
     -- match dir color with hidden dir color (background oil dir issue)
-    local comment_hl = vim.api.nvim_get_hl(0, { name = 'Comment' })
-
-    vim.api.nvim_set_hl(0, 'OilDir', { fg = comment_hl.fg, bg = 'none' })
-    vim.api.nvim_set_hl(0, 'OilDirHidden', { fg = '#6e6e66' })
-    vim.api.nvim_set_hl(0, 'OilFileHidden', { fg = '#6e6e66' })
+    -- local comment_hl = vim.api.nvim_get_hl(0, { name = 'Comment' })
+    --
+    -- vim.api.nvim_set_hl(0, 'OilDir', { fg = comment_hl.fg, bg = 'none' })
+    -- vim.api.nvim_set_hl(0, 'OilDirHidden', { fg = '#6e6e66' })
+    -- vim.api.nvim_set_hl(0, 'OilFileHidden', { fg = '#6e6e66' })
   end,
 }
