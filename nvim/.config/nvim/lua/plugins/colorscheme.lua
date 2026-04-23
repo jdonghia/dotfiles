c03ca0cb714b0ef6b5f9082041c782f2dbf1bebe
Joao Donghia <joao.donghia@gmail.com>
2026-04-23 17:22:38 -0300

    nvim: update colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,20 @@
+-- return {
+--   "Mofiqul/vscode.nvim",
+--   config = function()
+--     vim.cmd.colorscheme("vscode")
+--   end,
+-- }
+
 return {
-  "Mofiqul/vscode.nvim",
-  config = function()
-    vim.cmd.colorscheme("vscode")
+  "maxmx03/solarized.nvim",
+  lazy = false,
+  priority = 1000,
+  ---@type solarized.config
+  opts = {},
+  config = function(_, opts)
+    vim.o.termguicolors = true
+    vim.o.background = "dark"
+    require("solarized").setup(opts)
+    vim.cmd.colorscheme("solarized")
   end,
 }
