d6437c84c4e843987f4e07596be45c3299d2719d
Joao Donghia <joao.donghia@gmail.com>
2026-04-25 16:59:11 -0300

    update colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -4,17 +4,68 @@
 --     vim.cmd.colorscheme("vscode")
 --   end,
 -- }
+--
+-- return {
+--   "maxmx03/solarized.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   ---@type solarized.config
+--   opts = {},
+--   config = function(_, opts)
+--     vim.o.termguicolors = true
+--     vim.o.background = "dark"
+--     require("solarized").setup(opts)
+--     vim.cmd.colorscheme("solarized")
+--   end,
+-- }
+
+-- return
+-- {
+--   "rose-pine/neovim",
+--   name = "rose-pine",
+--   priority = 1000,
+--   lazy = false,
+--   config = function()
+--     -- if vim.o.background == "light" then
+--     --   vim.cmd.colorscheme("rose-pine-dawn")
+--     -- else
+--     --   vim.cmd.colorscheme("rose-pine-moon")
+--     -- end
+--     vim.cmd.colorscheme("rose-pine-moon")
+--   end,
+-- },
 
 return {
-  "maxmx03/solarized.nvim",
-  lazy = false,
-  priority = 1000,
-  ---@type solarized.config
-  opts = {},
-  config = function(_, opts)
-    vim.o.termguicolors = true
-    vim.o.background = "dark"
-    require("solarized").setup(opts)
-    vim.cmd.colorscheme("solarized")
-  end,
+  {
+    "maxmx03/solarized.nvim",
+    lazy = false,
+    priority = 1000,
+    ---@type solarized.config
+    opts = {},
+    config = function(_, opts)
+      vim.o.termguicolors = true
+      require("solarized").setup(opts)
+      vim.cmd.colorscheme("solarized")
+    end,
+  },
+
+  {
+    "cormacrelf/dark-notify",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      local dn = require("dark_notify")
+
+      dn.run({
+        onchange = function(mode)
+          if mode == "light" then
+            vim.o.background = "light"
+          elseif mode == "dark" then
+            vim.o.background = "dark"
+          end
+          -- end
+        end,
+      })
+    end,
+  },
 }
