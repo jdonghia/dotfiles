4013844197a8be1b9b7d3a90129b2ceb886560e5
Joao Donghia <joao.donghia@gmail.com>
2026-06-06 12:46:52 -0300

    add snacks isolated keymapping

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -4,5 +4,9 @@ return {
     opts = {
       scroll = { enabled = false },
     },
+    keys = {
+      { "<C-n>", LazyVim.pick("files", { hidden = true }), desc = "Find Files (Root Dir)" },
+      { "<leader>ps", LazyVim.pick("grep", { hidden = true }), desc = "Grep (Root Dir)" },
+    },
   },
 }
